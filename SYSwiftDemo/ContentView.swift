04c9ae80189a85391bea97ee122649fed8bf45d3
panshiyu <panshiyu@iicall.com>
2024-08-02 09:55:57 +0800

    创建项目
    Start to my swift project. Go!

---
+++ SYSwiftDemo/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SYSwiftDemo
+//
+//  Created by Pan skycol on 2024/8/2.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SYSwiftDemo/SYSwiftDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  SYSwiftDemoApp.swift
+//  SYSwiftDemo
+//
+//  Created by Pan skycol on 2024/8/2.
+//
+
+import SwiftUI
+
+@main
+struct SYSwiftDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
