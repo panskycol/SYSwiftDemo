72adb0fa80d349ae9888f532438dc6f4c1b1f302
panskycol <986825213@qq.com>
2024-08-07 23:51:22 +0800

    拉拽手势初调

--- SYSwiftDemo/UserCardView.swift
+++ SYSwiftDemo/UserCardView.swift
@@ -10,6 +10,7 @@ import SwiftUI
 struct UserCardView: View {
     var userCard: UserCard
     @State var imageIndex = 0
+    @State var offset:CGSize = .zero
     
     var body: some View {
         GeometryReader{ proxy in
@@ -20,7 +21,6 @@ struct UserCardView: View {
                 .cornerRadius(20)
             
             HStack{
-                
                 Rectangle()
                     .onTapGesture {
                         updateImageIndex(hasMoreImage: false)
@@ -41,12 +41,98 @@ struct UserCardView: View {
             }
             .padding(.top, 10)
             .padding(.horizontal)
+            HStack {
+                if offset.width > 0{
+                    creatUserCardLabel(title: "LIKE", degree: -20, color: .green)
+                } else if offset.width < 0{
+                    creatUserCardLabel(title: "NOPE", degree: 20, color: .red)
+                }
+            }
+            .padding(.horizontal, 30)
+            .padding(.top, 40)
+            Spacer()
+            createUserCardBottomInfo()
         }
+        .offset(offset)
+        .scaleEffect(getScaleAmount())
+        .rotationEffect(Angle(degrees: getScaleAmount()))
+        .gesture(
+            DragGesture()
+                .onChanged{ value in
+                    
+                    withAnimation(.easeOut(duration: 0.2)){
+                        offset = value.translation
+                    }
+                    print("拉拽了")
+                }
+                .onChanged{ value in
+                    withAnimation(.easeOut(duration: 0.2)){
+                        offset = .zero
+                    }
+                }
+        )
     }
     
     func updateImageIndex(hasMoreImage: Bool){
         let nextIndex = hasMoreImage ? imageIndex + 1 : imageIndex - 1;
         imageIndex = min(max(0, nextIndex), userCard.photos.count - 1)
+    }
+    
+    func creatUserCardLabel(title: String, degree: Double, color: Color) -> some View {
+        
+        Text(title)
+            .tracking(3)
+            .font(.title)
+            .fontWeight(.bold)
+            .padding(.horizontal)
+            .foregroundColor(color)
+            .overlay(
+                RoundedRectangle(cornerRadius: 5)
+                    .stroke(color, lineWidth: 3)
+            )
+            .rotationEffect(.degrees(degree))
+    }
+    
+    func getScaleAmount() -> CGFloat {
+        
+        let max = UIScreen.main.bounds.width/2.0
+        let currentAmount = abs(offset.width)
+        let percentage = currentAmount / max
+        return 1.0 - min(percentage, 0.7) * 0.3
+    }
+    
+    func createUserCardBottomInfo() -> some View {
+        HStack{
+            VStack(alignment: .leading, spacing: 5){
+                Text("\(userCard.name),\(userCard.age)")
+                    .font(.system(size: 30))
+                    .fontWeight(.heavy)
+                HStack{
+                    Text(userCard.zodiac)
+                        .fontWeight(.bold)
+                        .padding(5)
+                        .background(Color.white.opacity(0.3))
+                        .cornerRadius(5)
+                    Text(userCard.place)
+                }
+            }
+            Spacer()
+            Button{
+                
+            } label: {
+                Image(systemName: "info.circle.fill")
+                    .font(.system(size: 30))
+                    .padding(8)
+            }
+        }
+        .foregroundColor(.white)
+        .padding()
+        .padding(.bottom, 60)
+        .background(
+            LinearGradient(colors: [.black.opacity(0.9), .clear], startPoint: .bottom, endPoint: .top)
+        )
+        .cornerRadius(20)
+        .clipped()
     }
 }
 
